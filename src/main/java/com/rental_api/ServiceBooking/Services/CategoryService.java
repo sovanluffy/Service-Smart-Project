2ba2b446a2d4c5ff010f5556f6879a1fea5e0174
kimhong <try.kimhong@institute.pse.ngo>
2026-01-19 16:41:24 +0700

    finish create and get all category

---
+++ src/main/java/com/rental_api/ServiceBooking/Services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.rental_api.ServiceBooking.Services;
+
+import com.rental_api.ServiceBooking.Dto.Request.CategoryRequest;
+import com.rental_api.ServiceBooking.Dto.Response.CategoryResponse;
+import java.util.List;
+
+public interface CategoryService {
+
+    CategoryResponse create(CategoryRequest categoryRequest);
+
+    List<CategoryResponse> getAll();
+}
